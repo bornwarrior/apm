4bc8a83a4a26cd7d189f535ef94ed19aec9d0e4a
bornwarrior <j3patel@gmail.com>
2018-09-27 21:51:51 -0400

    First Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+import {Component } from '@angular/core'
+@Component ({
+  selector : 'pm-root',
+  template: `
+    <div><h1>{{pageTitle}}</h1>
+      <div>My First Component</div>
+    </div>
+     `
 })
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  pageTitle: string = 'Acme Product Management';
 }
