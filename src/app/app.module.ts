afeb3efaf0bc6c864898b8587a48c51d3c98a1e9
bornwarrior <j3patel@gmail.com>
2018-09-29 21:31:40 -0400

    Added filter by to show trings only (not filtering list yet)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './Products/product-list.component';
+import { fromEventPattern } from 'rxjs';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,8 @@ import { ProductListComponent } from './Products/product-list.component';
     ProductListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   bootstrap: [AppComponent]
 })
