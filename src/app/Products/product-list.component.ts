82fdb3603e517177d40d8f33e791d2b92271bdf9
bornwarrior <j3patel@gmail.com>
2018-10-09 21:22:36 -0400

    Retrieving Product List using HTTP

--- src/app/Products/product-list.component.ts
+++ src/app/Products/product-list.component.ts
@@ -13,6 +13,7 @@ export class ProductListComponent implements OnInit {
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
+    errorMessage: string;
 
     _listFilter: string;
     get listFilter(): string {
@@ -37,8 +38,15 @@ export class ProductListComponent implements OnInit {
         this.showImage = !this.showImage;
     }
     ngOnInit(): void {
-        this.products = this.productService.getProducts();
-        this.filteredProducts = this.products;
+        this.productService.getProducts().subscribe(
+            products => {
+                this.products = products;
+                this.filteredProducts = this.products;
+            },
+            error => this.errorMessage = <any>error
+        );
+
+
     }
 
     performFilter(filterBy: string): IProduct[] {
